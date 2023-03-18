1cb8d04437ade9d4f9cd3433935c333e9f83031f
kolibri753 <kolibri753@gmail.com>
2023-03-18 17:45:59 +0200

    Add TopNavigationComponent to WorkoutExercisesScreen

--- screens/WorkoutExercisesScreen.js
+++ screens/WorkoutExercisesScreen.js
@@ -1,100 +1,109 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import {
+	View,
+	Text,
+	StyleSheet,
+	FlatList,
+	TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { colors } from "../styles/colors";
-import { AntDesign } from '@expo/vector-icons'; 
-// import WorkoutExerciseScreen from "../screens/WorkoutExerciseScreen";
+import { AntDesign } from "@expo/vector-icons";
+import TopNavigationComponent from "../components/TopNavigationComponent";
 
 const WorkoutExercisesScreen = ({ route, navigation }) => {
-  const { workout } = route.params;
+	const { workout } = route.params;
 
-  const handlePlayButtonPress = () => {
-    console.log("WorkoutExercisesScreen", JSON.stringify(workout.exercises));
-    navigation.navigate("WorkoutExerciseScreen", {
-      exercises: workout.exercises,
-    });
-  };
-  
+	const handlePlayButtonPress = () => {
+		console.log("WorkoutExercisesScreen", JSON.stringify(workout.exercises));
+		navigation.navigate("WorkoutExerciseScreen", {
+			exercises: workout.exercises,
+		});
+	};
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.detailsContainer}>
-        <Text style={styles.title}>{workout.title}</Text>
-        <Text style={styles.description}>{workout.description}</Text>
-        <TouchableOpacity
-          onPress={handlePlayButtonPress}
-          style={styles.playButton}
-          activeOpacity={0.5}
-        >
-          <Text style={styles.playButtonText}>Play Workout</Text>
-          <AntDesign name="play" size={24} color="black" />
-        </TouchableOpacity>
-      </View>
-      <FlatList
-        data={workout.exercises}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item, index }) => (
-          <View style={styles.exerciseContainer}>
-            <Text style={styles.exerciseTitle}>
-              {index + 1}. {item.name}
-            </Text>
-          </View>
-        )}
-      />
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<TopNavigationComponent
+				title={`${workout.name}`}
+				activeDot={2}
+				navigation={navigation}
+			/>
+			<View style={styles.detailsContainer}>
+				<Text style={styles.description}>{workout.description}</Text>
+				<TouchableOpacity
+					onPress={handlePlayButtonPress}
+					style={styles.playButton}
+					activeOpacity={0.5}
+				>
+					<Text style={styles.playButtonText}>Play Workout</Text>
+					<AntDesign name="play" size={24} color="black" />
+				</TouchableOpacity>
+			</View>
+			<FlatList
+				data={workout.exercises}
+				keyExtractor={(item, index) => index.toString()}
+				renderItem={({ item, index }) => (
+					<View style={styles.exerciseContainer}>
+						<Text style={styles.exerciseTitle}>
+							{index + 1}. {item.name}
+						</Text>
+					</View>
+				)}
+			/>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.black,
-    padding: 20,
-  },
-  detailsContainer: {
-    alignItems: "center",
-  },
-  title: {
-    color: colors.white,
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 10,
-    textAlign: "center",
-  },
-  description: {
-    color: colors.white,
-    fontSize: 18,
-    marginBottom: 30,
-    textAlign: "center",
-  },
-  exerciseContainer: {
-    backgroundColor: colors.darkGray,
-    borderRadius: 10,
-    padding: 10,
-    marginBottom: 10,
-  },
-  exerciseTitle: {
-    color: colors.white,
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "left",
-  },
-  playButton: {
-    display: "flex",
-    flexDirection: "row",
-    gap: 14,
-    alignItems: "center",
-    backgroundColor: colors.yellow,
-    borderRadius: 10,
-    padding: 10,
-    marginBottom: 20,
-  },
-  playButtonText: {
-    color: colors.black,
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: colors.grey,
+		// padding: 20,
+	},
+	detailsContainer: {
+		alignItems: "center",
+	},
+	title: {
+		color: colors.white,
+		fontSize: 24,
+		fontWeight: "bold",
+		marginBottom: 10,
+		textAlign: "center",
+	},
+	description: {
+		color: colors.white,
+		fontSize: 18,
+		marginBottom: 30,
+		textAlign: "center",
+	},
+	exerciseContainer: {
+		backgroundColor: colors.darkGray,
+		borderRadius: 10,
+		padding: 10,
+		marginBottom: 10,
+	},
+	exerciseTitle: {
+		color: colors.white,
+		fontSize: 18,
+		fontWeight: "bold",
+		textAlign: "left",
+	},
+	playButton: {
+		display: "flex",
+		flexDirection: "row",
+		gap: 14,
+		alignItems: "center",
+		backgroundColor: colors.yellow,
+		borderRadius: 10,
+		padding: 10,
+		marginBottom: 20,
+	},
+	playButtonText: {
+		color: colors.black,
+		fontSize: 20,
+		fontWeight: "bold",
+		textAlign: "center",
+	},
 });
 
 export default WorkoutExercisesScreen;
